d4d6d1ef07734b405fcca5b7bb43dad748ab48bd
Matheus Marin <marinnmatheus@gmail.com>
2021-07-22 19:31:18 -0300

    add another extension function example

--- src/main/kotlin/com/kotlinbasics/extensionfunctions/ExtensionFunction.kt
+++ src/main/kotlin/com/kotlinbasics/extensionfunctions/ExtensionFunction.kt
@@ -1,9 +1,14 @@
 package com.kotlinbasics.extensionfunctions
 
 fun main() {
-    print("Matheus".sayHello())
+    println("Matheus".sayHello())
+    println(300.sum100())
 }
 
 fun String.sayHello(): String {
     return "Hello $this"
+}
+
+fun Int.sum100() : Int {
+    return this + 100
 }
