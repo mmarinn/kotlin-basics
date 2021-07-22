e039a92de92a36c60cd31fcdde13d89316a77998
Matheus Marin <marinnmatheus@gmail.com>
2021-07-22 19:21:47 -0300

    add when example

---
+++ src/main/kotlin/com/kotlinbasics/classesandfunctions/When.kt
@@ -0,0 +1,11 @@
+package com.kotlinbasics.classesandfunctions
+
+fun main() {
+    val x = 10
+    when (x) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> print("x is neither 1 nor 2")
+
+    }
+}
